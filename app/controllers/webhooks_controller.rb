da142a30881b99bc1fc4212e62bdb67bcd7d9c3a
Stuart Chaney <stuartchaney22@gmail.com>
2011-10-28 08:59:43 -0700

    hurlin

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,23 @@
+class WebhooksController < ApplicationController
+
+   protect_from_forgery :except => :create
+  
+  #POST
+  def create
+	 	body = params.except(:action, :controller).first.first
+		#parsed_body = body.to_json
+		puts body.class
+		puts body.inspect
+
+		#puts "under here"
+		#puts body.to_json
+		#puts "TESTIN"
+		#puts body.to_json.first
+		#puts "PARAMS UNDER HERE"
+		#puts params.to_json
+    render :nothing => true
+    head :ok			
+	end
+ 
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hurl::Application.config.secret_token = '1e4a5e2196d50d37d213a6399d7424a7dd957d1dec8650cafeadb8d5cf06cbdaa099541bf762723e7ec62245ca3674e1c703d53f57e25b773dbed50afee36cee'
